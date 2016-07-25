dc4bbdda268d812f96a1380dead743c7a54310ef
thomas-yancey <tomyncy618@gmail.com>
2016-07-25 15:56:42 -0400

    disabled cors for development, working year filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
 
   def index
-    query = "SELECT * FROM movies WHERE release_year > 1972 ORDER BY metascore DESC NULLS LAST"
+    query = "SELECT * FROM movies
+             WHERE release_year > #{params[:start_year]}
+             ORDER BY metascore
+             DESC NULLS LAST"
     movies = Movie.paginate_by_sql(query, :page => params[:page], :per_page => 30)
     render json:  {
       current_page: movies.current_page,
