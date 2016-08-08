4912d0bf22422292aa6eefe1488e614973ac7d29
thomas-yancey <tomyncy618@gmail.com>
2016-08-07 23:42:47 -0400

    clean up movie_database model, create task to add all genres once

--- app/models/movie_database.rb
+++ app/models/movie_database.rb
@@ -1,21 +1,23 @@
 class MovieDatabase
 include HTTParty
-  attr_accessor :omdb_id, :response, :key, :movie
+  attr_accessor :response, :key, :movie
 
   base_uri "https://api.themoviedb.org/3"
   def initialize(params={})
       @key = ENV["MOVIEDB_KEY"]
-      @response = nil
-      @omdb_id = params[:omdb_id] || ""
       @movie = params[:movie]
+      @response = nil
   end
 
   def call_general_info
-    self.class.get("/movie/#{self.omdb_id}?api_key=#{self.key}")
+    self.class.get("/movie/#{self.movie.omdb_id}?api_key=#{self.key}")
+  end
+
+  def call_movie_videos
+    self.class.get("/movie/#{self.movie.omdb_id}/videos?api_key=#{self.key}")
   end
 
   def make_call_and_collect_imdb_and_genres
-    self.call_general_info
     self.response = self.call_general_info
     if self.response["imdb_id"]
       movie.update_attributes(imdb_id: self.response["imdb_id"])
@@ -26,21 +28,19 @@ include HTTParty
   end
 
   def collect_genres
-    if self.response["genres"].length > 0
+    if !!self.response["genres"] && self.response["genres"].length > 0
       self.response["genres"].each do |genre|
         curr_genre = Genre.find_by(moviedb_id: genre["id"])
-        if curr_genre
-          movie.genres << curr_genre
-        else
-          curr_genre = Genre.create(moviedb_id: genre["id"], name: genre["name"])
-          movie.genres << curr_genre
+        if !self.movie.genres.find_by(moviedb_id: genre["id"])
+          if curr_genre
+            self.movie.genres << curr_genre
+          else
+            curr_genre = Genre.create(moviedb_id: genre["id"], name: genre["name"])
+            self.movie.genres << curr_genre
+          end
         end
       end
     end
-  end
-
-  def call_movie_videos
-    self.class.get("/movie/#{self.omdb_id}/videos?api_key=#{self.key}")
   end
 
   def set_trailer_response
@@ -57,28 +57,9 @@ include HTTParty
     nil
   end
 
-  def get_trailer_data
-    self.set_trailer_response
-    grab_key_from_response
-  end
-
   def add_trailer_key_to_movie
     self.set_trailer_response
     self.movie.update_attributes(trailer: self.grab_key_from_response)
-  end
-
-  # Only used for initial pull of data
-  def self.grab_key_for_all_movies_with_imdb_id
-    movie_db_object = self.new
-    movies = Movie.all
-    movies.each_with_index do |movie, idx|
-      next if !movie.trailer.nil?
-      next if (movie.imdb_id.length == 0 && movie.omdb_id == 0)
-      movie_db_object.imdb_id = movie.imdb_id.length == 0 ? movie.omdb_id : movie.imdb_id
-      youtube_key = movie_db_object.get_trailer_data
-      movie.update_attributes(trailer: youtube_key)
-      sleep 0.2
-    end
   end
 
 end
--- lib/tasks/nightly_jobs.rake
+++ lib/tasks/nightly_jobs.rake
@@ -1,24 +1,36 @@
 task :create_new_movies => :environment do
-  
   guidebox = Guidebox.new
   guidebox.create_and_store_new_movies
   new_movies = guidebox.new_movies_array
   # grab movie db info and omdb info
   new_movies.each do |movie_id|
     movie = Movie.find(movie_id)
-    omdb_id = movie.omdb_id || movie.imdb_id
-    movie_db = MovieDatabase.new({movie: movie, omdb_id: omdb_id})
-    movie_db.add_trailer_key_to_movie
-    movie_db.make_call_and_collect_imdb_and_genres
-    if movie.imdb_id && movie.imdb_id.length > 0
-      omdb = Omdb.new({movie: movie, imdb_id: movie.imdb_id})
-      omdb.make_call_and_collect_all_data
+
+    if !movie.omdb_id.nil? && movie.omdb_id > 0
+
+      movie_db = MovieDatabase.new({movie: movie})
+      movie_db.add_trailer_key_to_movie
+      movie_db.make_call_and_collect_imdb_and_genres
+
+      if movie.imdb_id && movie.imdb_id.length > 0
+        omdb = Omdb.new({movie: movie, imdb_id: movie.imdb_id})
+        omdb.make_call_and_collect_all_data
+      end
+
     end
   end
-
 end
 
 task :update_changed_movies => :environment do
   guidebox = Guidebox.new
   guidebox.change_update_movie_sources
+end
+
+task :add_genres_to_all_movies_once => :environment do
+  Movie.where.not(omdb_id: 0).each do |movie|
+    sleep 0.25
+    movie_db = MovieDatabase.new({movie: movie})
+    movie_db.response = movie_db.call_general_info
+    movie_db.collect_genres
+  end
 end
